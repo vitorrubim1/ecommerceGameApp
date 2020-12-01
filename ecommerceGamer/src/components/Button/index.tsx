4d0080747c3b526edd847fef07b5268585febf01
Vitor Rubim <vitorrubim1@hotmail.com>
2020-12-01 20:06:13 -0300

    Feat: Adicionando um botao para comprar/adicionar no carrinho. Com styles separado

---
+++ ecommerceGamer/src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+
+import { styles } from "./styles";
+
+export default function Button() {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Comprar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
---
+++ ecommerceGamer/src/components/Button/styles.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center"
+  },
+
+  button: {
+    width: "90%",
+    height: 50,
+    backgroundColor: "#17181a",
+    borderRadius: 5,
+    marginVertical: "5%",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+
+  buttonText: {
+    fontSize: 20,
+    color: "#ebe8e8",
+    fontFamily: "Roboto_700Bold"
+  },
+});
